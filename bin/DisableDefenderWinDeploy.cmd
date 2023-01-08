266ed8565a0f8778a74666fd6a01dce1e6497097
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2023-01-08 17:49:51 +0100

    Bug fix

--- bin/DisableDefenderWinDeploy.cmd
+++ bin/DisableDefenderWinDeploy.cmd
@@ -2,7 +2,7 @@
 title Disable Windows Defender - Batch Edition
 echo Disabling Windows Defender...
 sc config WinDefend start=disabled
-reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /v "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
 echo Final touches...
 
 reg add "HKLM\System\Setup" /v "CmdLine" /t REG_SZ /d "" /f
